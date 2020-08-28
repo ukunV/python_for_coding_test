d5040ce137c6beebae768ef9852d6d59267b56ef
kunukkk <showklee@naver.com>
2020-08-28 18:54:55 +0900

    chapter6_sort_1

---
+++ Chapter6_Sort_1.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <b>selection sort
+
+# In[1]:
+
+
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in range(len(array)):
+    min_index = i
+    for j in range(i + 1, len(array)):
+        if array[min_index] > array[j]:
+            min_index = j
+    array[i], array[min_index] = array[min_index], array[i]
+    
+print(array)
+
+
+# <b>insertion sort
+
+# In[2]:
+
+
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in range(1, len(array)):
+    for j in range(i, 0, -1):
+        if array[j] < array[j - 1]:
+            array[j], array[j - 1] = array[j - 1], array[j]
+        else:
+            break
+            
+print(array)
+
+
+# <b>quick sort
+
+# In[4]:
+
+
+array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
+
+def quick_sort(array, start, end):
+    if start >= end:
+        return
+    pivot = start
+    left = start + 1
+    right = end
+    while left <= right:
+        while left <= end and array[left] <= array[pivot]:
+            left += 1
+        while right > start and array[right] >= array[pivot]:
+            right -= 1
+        if left > right:
+            array[right], array[pivot] = array[pivot], array[right]
+        else:
+            array[left], array[right] = array[right], array[left]
+    quick_sort(array, start, right - 1)
+    quick_sort(array, right + 1, end)
+    
+quick_sort(array, 0, len(array) - 1)
+print(array)
+
+
+# <b>quick sort (python_ver)  => similar performance with sort(), sorted()
+
+# In[7]:
+
+
+array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
+
+def quick_sort(array):
+    # 리스트가 하나 이하의 원소만을 담고 있다면 종료
+    if len(array) <= 1:
+        return array
+    
+    pivot = array[0] # 피벗은 첫번째 원소
+    tail = array[1:] # 피벗을 제외한 리스트
+    
+    left_side = [x for x in tail if x <= pivot] # 분할된 왼쪽 부분
+    right_side = [x for x in tail if x > pivot] # 분할된 오른쪽 부분
+    
+    # 분할 이후 왼쪽 부분과 오른쪽 부분에서 각각 정렬을 수행하고 전체 리스트를 반환
+    return quick_sort(left_side) + [pivot] + quick_sort(right_side)
+
+print(quick_sort(array))
+
+
+# <b>count sort
+
+# In[8]:
+
+
+array = [7, 5, 9, 0, 3, 1, 6, 2, 9, 1, 4, 8, 0, 5, 2]
+
+count = [0] * (max(array) + 1)
+
+for i in range(len(array)):
+    count[array[i]] += 1
+
+for i in range(len(count)):
+    for j in range(count[i]):
+        print(i, end = ' ')
+
