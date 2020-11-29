72846b2c31dd687cbfbf28a10e60d04c98b65535
kunukkk <showklee@naver.com>
2020-11-29 20:57:48 +0900

    20/11/29_update
    20/11/29_update

--- 이것이 코딩테스트다 with 파이썬/Chapter3_그리디.py
+++ 이것이 코딩테스트다 with 파이썬/Chapter3_그리디.py
@@ -3,27 +3,30 @@
 
 # <b>예제 3-1. 거스름돈
 
+# ans)
+
 # In[2]:
 
 
 n = 1260
 count = 0
+# 큰 단위의 화폐부터 차례대로 확인 (그리디)
 list = [500, 100, 50, 10]
 
 for coin in list:
-    count += n // coin
+    count += n // coin # 해당 화폐로 거슬러 줄 수 있는 동전의 개수 세기
     n %= coin
-    
+
 print(count)
 
 
 # <b>큰 수의 법칙
 
-# In[19]:
+# In[2]:
 
 
 n,m,k = map(int, input().split())
-nums = [2,4,5,4,6]
+nums = list(map(int, input().split()))
 
 nums.sort()
 f = nums[n-1]
@@ -36,7 +39,7 @@ res += f * (m%(k+1))
 print(res)
 
 
-# In[13]:
+# In[4]:
 
 
 n,m,k=map(int, input().split())
@@ -62,26 +65,34 @@ while True:
 print(result)
 
 
-# In[21]:
+# ans)
+
+# In[3]:
 
 
+# N, M, K를 공백으로 구분하여 입력받기
 n,m,k=map(int, input().split(' '))
+# N개의 수를 공백으로 구분하여 입력받기
 data=list(map(int, input().split(' ')))
 
-data.sort()
-first=data[n-1]
-second=data[n-2]
+data.sort() # 입력받은 수 정렬
+first=data[n-1] # 가장 큰 수
+second=data[n-2] # 두 번째로 큰 수
 
+# 가장 큰 수가 더해지는 횟수 계산
 count = (m//(k+1))*k
 count += m%(k+1)
 
 result = 0
-result += count * first
-result += (m-count) * second
+result += count * first # 가장 큰 수 더하기
+result += (m-count) * second # 두 번째로 큰 수 더하기
+
 print(result)
 
 
 # <b>숫자 카드 게임
+
+# ans)
 
 # In[15]:
 
@@ -89,18 +100,36 @@ print(result)
 n,m=map(int, input().split())
 card = []
 
-for i in range(n):
-    t = []
-    for j in range(m):
-        t.append(int(input()))
-    card.append(t)
-
 result = 0
+# 한 줄씩 입력받아 확인
 for i in range(n):
-    min_v = min(card[i])
-    result = max(result, min_v)
-    
-print(result)    
+    data = list(map(int, input().split()))
+    # 현재 줄에서 '가장 작은 수' 찾기
+    min_value = min(data)
+    # '가장 작은 수'들 중에서 가장 큰 수 찾기
+    result = max(result, min_value)
+
+print(result)
+
+
+# In[ ]:
+
+
+# N, M를 공백으로 구분하여 입력받기
+n,m=map(int, input().split())
+result = 0
+
+# 한 줄씩 입력받아 확인
+for i in range(n):
+    data = list(map(int, input().split()))
+    # 현재 줄에서 '가장 작은 수' 찾기
+    min_value = 10001
+    for a in data:
+        min_value = min(min_value, a)
+    # '가장 작은 수'들 중에서 가장 큰 수 찾기
+    result = max(result, min_value)
+
+print(result)
 
 
 # <b>1이 될 때까지
@@ -142,6 +171,8 @@ while n > 1:
 print(result)
 
 
+# ans)
+
 # In[30]:
 
 
@@ -149,14 +180,18 @@ n, k = map(int, input().split())
 result = 0
 
 while True:
+    # (N == K로 나누어 떨어지는 수)가 될 때까지 1씩 빼기
     target = (n // k) * k
     result += (n - target)
     n = target
+    # N이 K보다 작을 때(더 이상 나눌 수 없을 때) 반복문 탈출
     if n < k:
         break
+    # K로 나누기
     result += 1
     n //= k
 
+# 마지막으로 남은 수에 대하여 1씩 빼기
 result += (n - 1)
 print(result)
 
