beb00e11a921da65aa855eccc6892a2c1412d508
kunukkk <showklee@naver.com>
2020-12-05 12:12:01 +0900

    20/12/05_update
    20/12/05_update

--- 이것이 코딩테스트다 with 파이썬/Chapter6_Sort_1.py
+++ 이것이 코딩테스트다 with 파이썬/Chapter6_Sort_1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# <b>selection sort
+# <b>selection sort - O(n^2)
 
 # In[1]:
 
@@ -9,16 +9,16 @@
 array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
 
 for i in range(len(array)):
-    min_index = i
+    min_index = i # 가장 작은 원소의 인덱스
     for j in range(i + 1, len(array)):
         if array[min_index] > array[j]:
             min_index = j
-    array[i], array[min_index] = array[min_index], array[i]
+    array[i], array[min_index] = array[min_index], array[i] # Swap
     
 print(array)
 
 
-# <b>insertion sort
+# <b>insertion sort - O(n^2) (almost sorted - O(n))
 
 # In[2]:
 
@@ -26,10 +26,10 @@ print(array)
 array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
 
 for i in range(1, len(array)):
-    for j in range(i, 0, -1):
-        if array[j] < array[j - 1]:
+    for j in range(i, 0, -1): # 인덱스 i부터 1까지 감소하며 반복하는 문법
+        if array[j] < array[j - 1]: # 한 칸씩 왼쪽으로 이동
             array[j], array[j - 1] = array[j - 1], array[j]
-        else:
+        else: # 자기보다 작은 데이터를 만나면 그 위치에서 멈춤
             break
             
 print(array)
@@ -43,20 +43,23 @@ print(array)
 array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
 
 def quick_sort(array, start, end):
-    if start >= end:
+    if start >= end: # 원소가 1개인 경우 종료
         return
-    pivot = start
+    pivot = start # pivot은 첫 번째 원소
     left = start + 1
     right = end
     while left <= right:
+        # pivot보다 큰 데이터를 찾을 때까지 반복
         while left <= end and array[left] <= array[pivot]:
             left += 1
+        # pivot보다 작은 데이터를 찾을 때까지 반복
         while right > start and array[right] >= array[pivot]:
             right -= 1
-        if left > right:
+        if left > right: # 엇갈렸다면 작은 right -= 1 데이터와 pivot을 교체
             array[right], array[pivot] = array[pivot], array[right]
-        else:
+        else: # 엇갈리지 않았다면 작은 데이터와 큰 데이터를 교체
             array[left], array[right] = array[right], array[left]
+    # 분할 이후 왼쪽 부분과 오른쪽 부분에서 각각 정렬 수행
     quick_sort(array, start, right - 1)
     quick_sort(array, right + 1, end)
     
@@ -64,7 +67,7 @@ quick_sort(array, 0, len(array) - 1)
 print(array)
 
 
-# <b>quick sort (python_ver)  => similar performance with sort(), sorted()
+# <b>quick sort (python_ver) - O(NlogN) => similar performance with sort(), sorted()
 
 # In[7]:
 
@@ -76,8 +79,8 @@ def quick_sort(array):
     if len(array) <= 1:
         return array
     
-    pivot = array[0] # 피벗은 첫번째 원소
-    tail = array[1:] # 피벗을 제외한 리스트
+    pivot = array[0] # pivot은 첫번째 원소
+    tail = array[1:] # pivot을 제외한 리스트
     
     left_side = [x for x in tail if x <= pivot] # 분할된 왼쪽 부분
     right_side = [x for x in tail if x > pivot] # 분할된 오른쪽 부분
@@ -88,19 +91,36 @@ def quick_sort(array):
 print(quick_sort(array))
 
 
-# <b>count sort
+# <b>count sort - O(N+K)
 
 # In[8]:
 
 
+# 모든 원소의 값이 0보다 크거나 같다고 가정
 array = [7, 5, 9, 0, 3, 1, 6, 2, 9, 1, 4, 8, 0, 5, 2]
-
+# 모든 범위를 포함하는 리스트 선언(모든 값은 0으로 초기화)
 count = [0] * (max(array) + 1)
 
 for i in range(len(array)):
-    count[array[i]] += 1
+    count[array[i]] += 1 # 각 데이터에 해당하는 인덱스의 값 증가
 
-for i in range(len(count)):
+for i in range(len(count)): # 리스트에 기록된 정렬 정보 확인
     for j in range(count[i]):
-        print(i, end = ' ')
+        print(i, end = ' ') # 띄어쓰기를 구분으로 등장한 횟수만큼 인덱스 출력
+
+
+# <b> ※ sorted(), sort() - O(NlogN)
+
+# <b> key를 활용한 소스코드 - O(NlogN)
+
+# In[1]:
+
+
+array = [('가', 2), ('나', 5), ('다', 3)]
+
+def setting(data):
+    return data[1]
+
+result = sorted(array, key=setting)
+print(result)
 
