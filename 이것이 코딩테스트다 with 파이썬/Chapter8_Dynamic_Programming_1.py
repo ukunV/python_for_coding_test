0ae17b2e9e33926d9380561326916b9e092f9782
kunukkk <showklee@naver.com>
2020-12-08 11:52:31 +0900

    20/12/08_update
    20/12/08_update

---
+++ 이것이 코딩테스트다 with 파이썬/Chapter8_Dynamic_Programming_1.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <b> 피보나치 함수 소스코드 - O(2^n)
+
+# In[1]:
+
+
+# 피보나치 함수(Fibonacci Function)를 재귀 함수로 구현
+def fibo(x):
+    if x == 1 or x == 2:
+        return 1
+    return fibo(x - 1) + fibo(x - 2)
+
+print(fibo(4))
+
+
+# <b> 피보나치 수열 소스코드(재귀적) - O(N)
+
+# In[2]:
+
+
+# 한 번 계산된 결과를 메모이제이션(Memoization)하기 위한 리스트 초기화
+d = [0] * 100
+
+# 피보나치 함수(Fibonacci Function)를 재귀함수로 구현(탑다운 다이나믹 프로그래밍)
+def fibo(x):
+    if x == 1 or x == 2:
+        return 1
+    # 이미 계산한 적 있는 문제라면 그대로 반환
+    if d[x] != 0:
+        return d[x]
+    # 아직 계산하지 않은 문제라면 점화식에 따라서 피보나치 결과 반환
+    d[x] = fibo(x - 1) + fibo(x - 2)
+    return d[x]
+
+print(fibo(99))
+
+
+# <b> 호출되는 함수 확인
+
+# In[5]:
+
+
+d = [0] * 100
+
+def pibo(x):
+    print('f(' + str(x) + ')', end=' ')
+    if x == 1 or x == 2:
+        return 1
+    if d[x] != 0:
+        return d[x]
+    d[x] = pibo(x - 1) + pibo(x - 2)
+    return d[x]
+
+pibo(6)    
+
+
+# <b> 피보나치 수열 소스코드(반복적)
+
+# In[6]:
+
+
+# 앞서 계산된 결과를 저장하기 위한 DP 테이블 초기화
+d = [0] * 100
+
+# 첫 번째 피보나치 수와 두 번째 피보나치 수는 1
+d[1] = 1
+d[2] = 1
+n = 99
+
+# 피보나치 함수(Fibonacci Function) 반복문으로 구현(보텀업 다이나믹 프로그래밍)
+for i in range(3, n+1):
+    d[i] = d[i - 1] + d[i - 2]
+    
+print(d[n])
+
+
+# <b> 1로 만들기
+
+# ans)
+
+# In[7]:
+
+
+# 정수 X를 입력받기
+x = int(input())
+
+# 앞서 계산된 결과를 저장하기 위한 DP 테이블 초기화
+d = [0] * 30001
+
+# 다이나믹 프로그래밍(Dynamic Programming) 진행(보텀업)
+for i in range(2, x + 1):
+    # 현재의 수에서 1을 빼는 경우
+    d[i] = d[i - 1] + 1
+    # 현재의 수가 2로 나누어 떨어지는 경우
+    if i % 2 == 0:
+        d[i] = min(d[i], d[i // 2] + 1)
+    # 현재의 수가 3으로 나누어 떨어지는 경우
+    if i % 3 == 0:
+        d[i] = min(d[i], d[i // 3] + 1)
+    # 현재의 수가 5로 나누어 떨어지는 경우
+    if i % 5 == 0:
+        d[i] = min(d[i], d[i // 5] + 1)
+        
+print(d[x])
+
