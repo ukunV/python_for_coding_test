2f407e9a4d841d357d71c0d6500288a56427ac74
kunukkk <showklee@naver.com>
2020-12-10 17:13:40 +0900

    20/12/10_update
    20/12/10_update

---
+++ 이것이 코딩테스트다 with 파이썬/Chapter8_Dynamic_Programming_2.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <b> 개미 전사
+
+# In[1]:
+
+
+# 정수 N을 입력받기
+n = int(input())
+# 모든 식량 정보 입력받기
+array = list(map(int, input().split()))
+
+# 앞서 계산된 결과를 저장하기 위한 DP 테이블 초기화
+d = [0] * 100
+
+# 다이나믹 프로그래밍(Dynamic Programming) 진행(보텀업)
+d[0] = array[0]
+d[1] = max(array[0], array[1])
+for i in range(2, n):
+    d[i] = max(d[i - 1], d[i - 2] + array[i])
+    
+# 계산된 결과 출력
+print(d[n - 1])
+
+
+# <b> 바닥 공사
+
+# In[2]:
+
+
+# 정수 N을 입력받기
+n = int(input())
+
+# 앞서 계산된 결과를 저장하기 위한 DP 테이블 초기화
+d[0] * 1001
+
+# 다이나믹 프로그래밍(Dynamic Programming) 진행(보텀업)
+d[1] = 1
+d[2] = 3
+for i in range(3, n+1):
+    d[i] = (d[i - 1] + 2 * d[i - 2]) % 796796
+    
+# 계산된 결과 출력
+print(d[n])
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
