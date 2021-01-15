6deb243065f1290e740842305c1550ac660d1986
ukunV <showklee@naver.com>
2021-01-15 18:55:38 +0900

    21/01/15_update
    21/01/15_update

--- 이것이 코딩테스트다 with 파이썬/Chapter09_Shortest_Path.py
+++ 이것이 코딩테스트다 with 파이썬/Chapter09_Shortest_Path.py
@@ -3,7 +3,7 @@
 
 # <b> 간단한 다익스트라 알고리즘 소스코드 - O(V^2) (V: 노드의 개수)
 
-# In[8]:
+# In[3]:
 
 
 INF = int(1e9) # 무한을 의미하는 값으로 10억을 설정
@@ -24,6 +24,8 @@ for _ in range(m):
     a, b, c = map(int, input().split())
     # a번 노드에서 b번 노드로 가는 비용이 c라는 의미
     graph[a].append((b, c))
+
+print(graph) # graph 상태확인
 
 # 방문하지 않은 노드 중에서 가장 최단 거리가 짧은 노드의 번호를 반환
 def get_smallest_node():
@@ -101,12 +103,14 @@ distance = [INF] * (n + 1)
 for _ in range(m):
     a, b, c = map(int, input().split())
     # a번 노드에서 b번 노드로 가는 비용이 c라는 의미
-    graph[a].append(b, c)
+    graph[a].append((b, c))
     
+print(graph) # graph 상태확인
+
 def dijkstra(start):
     q = []
     # 시작 노드로 가기 위한 최단 경로는 0으로 설정하여, 큐에 삽입
-    heapq.heapqpush(q, (0, start))
+    heapq.heappush(q, (0, start))
     distance[start] = 0
     while q:# 큐가 비어있지 않다면
         # 가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
