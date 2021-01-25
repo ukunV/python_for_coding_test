6242097da561cbaae0336f9cb847a028fd7a4776
ukunV <showklee@naver.com>
2021-01-25 16:16:41 +0900

    21/01/25_update
    21/01/25_update

--- this_is_coding_test/Chapter12_Implementation_problems.py
+++ this_is_coding_test/Chapter12_Implementation_problems.py
@@ -382,8 +382,85 @@ print(result)
 
 # <b> 14. 외벽 점검
 
-# In[ ]:
+# ans)
+
+# In[10]:
 
 
+from itertools import permutations
 
+def solution(n, weak, dist):
+    # 길이를 2배로 늘려서 '원형'을 일자 형태로 변형
+    length = len(weak)
+    for i in range(length):
+        weak.append(weak[i] + n)
+    print(weak)
+    answer = len(dist) + 1 # 투입할 친구 수의 최솟값을 찾아야 하므로 len(dist) + 1로 초기화
+    # 0부터 length - 1까지의 위치를 각각 시작점으로 설정
+    for start in range(length):
+        # 친구를 나열하는 모든 경우의 수 각각에 대하여 확인
+        for friends in list(permutations(dist, len(dist))):
+            count = 1 # 투칩할 친구의 수
+            # 해당 친구가 점검할 수 있는 마지막 위치
+            position = weak[start] + friends[count - 1]
+            # 시작점부터 모든 취약 지점을 확인
+            for index in range(start, start + length):
+                # 점검할 수 있는 위치를 벗어나는 경우
+                if position < weak[index]:
+                    count += 1 # 새로운 친구를 투입
+                    if count > len(dist): # 더 투입이 불가능하다면 종료
+                        break
+                    position = weak[index] + friends[count - 1]
+            answer = min(answer, count) # 최솟값 계산
+    if answer > len(dist):
+        return -1
+    return answer
+        
+n = int(input())
+weak = list(map(int, input().split()))
+dist = list(map(int, input().split()))
+
+print(solution(n, weak, dist))
+
+
+# In[9]:
+
+
+def solution(n, weak, dist):
+
+    W, F = len(weak), len(dist)
+    repair_lst = [()]  # 현재까지 고칠 수 있는 취약점들 저장 (1,2,3)
+    count = 0  # 투입친구 수
+    dist.sort(reverse=True) # 움직일 수 있는 거리가 큰 친구 순서대로
+
+    # 고칠 수 있는 것들 리스트 작성
+    for can_move in dist:
+        repairs = []  # 친구 별 고칠 수 있는 취약점들 저장
+        count += 1
+
+        # 수리 가능한 지점 찾기
+        for i, wp in enumerate(weak):
+            start = wp  # 각 위크포인트부터 시작
+            ends = weak[i:] + [n+w for w in weak[:i]]  # 시작점 기준 끝 포인트 값 저장
+            can = [end % n for end in ends if end -
+                   start <= can_move]  # 가능한 지점 저장
+            repairs.append(set(can))
+
+        # 수리 가능한 경우 탐색
+        cand = set()
+        for r in repairs:  # 새친구의 수리가능 지점
+            for x in repair_lst:  # 기존 수리가능 지점
+                new = r | set(x)  # 새로운 수리가능 지점
+                if len(new) == W:  # 모두 수리가능 한 경우 친구 수 리턴
+                    return count
+                cand.add(tuple(new))
+        repair_lst = cand
+
+    return -1
+
+n = int(input())
+weak = list(map(int, input().split()))
+dist = list(map(int, input().split()))
+
+print(solution(n, weak, dist))
 
