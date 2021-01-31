407fca83a52c756049e0f3e3ba6469c80b3086f7
ukunV <showklee@naver.com>
2021-01-31 21:46:49 +0900

    21/01/31_update
    21/01/31_update

--- this_is_coding_test/Chapter14_Sorting_problems.py
+++ this_is_coding_test/Chapter14_Sorting_problems.py
@@ -22,18 +22,85 @@ for student in students:
 
 # <b> 24. 안테나
 
+# In[9]:
+
+
+n = int(input())
+location = list(map(int, input().split()))
+location.sort()
+
+tar = (n - 1) // 2
+print(location[tar])
+
+
+# ans)
+
 # In[ ]:
 
 
+n = int(input())
+data = list(map(int, input().split()))
+data.sort()
 
+# 중간값 (median)을 출력
+print(data[(n - 1) // 2])
 
 
 # <b> 25. 실패율
 
-# In[ ]:
+# In[10]:
 
 
+n = int(input())
+stages = list(map(int, input().split()))
+stages.sort()
+f_rate = []
 
+for i in range(1, max(stages)):
+    temp = stages.count(i) / len(stages)
+    for j in range(len(stages)):
+        if stages[0] == i:
+            stages.remove(i)
+        else:
+            break
+    f_rate.append((i, temp))
+
+f_rate = sorted(f_rate, key=lambda t: t[1], reverse=True)
+
+for rate in f_rate:
+    print(rate[0], end= ' ')
+
+
+# ans)
+
+# In[7]:
+
+
+def solution(N, stages):
+    answer = []
+    length = len(stages)
+    
+    # 스테이지 번호를 1부터 N까지 증가시키며
+    for i in range(1, N + 1):
+        # 해당 스테이지에 머물러 있는 사람의 수 계산
+        count = stages.count(i)
+        
+        # 실패율 계산
+        if length == 0:
+            fail = 0
+        else:
+            fail = count / length
+            
+        # 리스트에 (스테이지 번호, 실패율) 원소 삽입
+        answer.append((i, fail))
+        length -= count
+        
+    # 실패율 기준으로 각 스테이지를 내림차순 정렬
+    answer = sorted(answer, key=lambda t: t[1], reverse=True)
+    
+    #  정렬된 스테이지 번호 출력
+    answer = [i[0] for i in answer]
+    return answer
 
 
 # <b> 26. 카드 정렬하기
