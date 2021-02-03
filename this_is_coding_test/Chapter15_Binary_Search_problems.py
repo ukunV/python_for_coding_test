c7ef526961b79284bd7275ad5b6d4c53d5989c0c
ukunV <showklee@naver.com>
2021-02-03 16:54:46 +0900

    21/02/03_update
    21/02/03_update

--- this_is_coding_test/Chapter15_Binary_Search_problems.py
+++ this_is_coding_test/Chapter15_Binary_Search_problems.py
@@ -154,16 +154,101 @@ else:
 
 # <b> 29. 공유기 설치
 
+# ans)
+
 # In[ ]:
 
 
+## 집의 개수(N)와 공유기의 개수(C)를 입력받기
+n,c = list(map(int, input().split()))
 
+# 전체 집의 좌표 정보를 입력받기
+array = []
+for _ in range(n):
+    array.append(int(input()))
+array.sort() # 이진 탐색 수행을 위해 정렬 수행
+    
+start = array[1] - array[0] # 집 gap 중에 가장 작은 값
+end = array[-1] - array[0] # 집 gap 중에 가장 큰 값
+result = 0
+    
+while(start <= end):
+    mid = (start + end) // 2 # mid는 가장 인접한 두 공유기 사이의 거리(gap)를 의미
+    value = array[0]
+    count = 1
+    # 현재의 mid값을 이용해 공유기를 설치
+    for i in range(1, n): # 앞에서부터 차근차근 설치
+        if array[i] >= value + mid:
+            value = array[i]
+            count += 1
+    if count >= c: # C개 이상의 공유기를 설치할 수 있는 경우, 거리를 증가
+        start = mid + 1
+        result = mid # 최적의 결과를 저장
+    else: # C개 이상의 공유기를 설치할 수 없는 경우, 거리를 감소
+        end = mid - 1
+            
+print(result)
 
 
 # <b> 30. 가사 검색
 
-# In[ ]:
+# In[9]:
 
 
+from bisect import bisect_left, bisect_right
 
+def count_by_range(a, left_value, right_value):
+    right_index = bisect_right(a, right_value)
+    left_index = bisect_left(a, left_value)
+    print(left_index, right_index)
+    
+words = ["frodo", "front", "frost", "frame", "kakao"]
+words.sort()
+print(words)
+
+count_by_range(words, 'froaa', 'frozz')
+
+
+# ans)
+
+# In[5]:
+
+
+from bisect import bisect_left, bisect_right
+
+# 값이 [left_value, right_value]인 데이터의 개수를 반환하는 함수
+def count_by_range(a, left_value, right_value):
+    right_index = bisect_right(a, right_value)
+    left_index = bisect_left(a, left_value)
+    return right_index - left_index
+
+# 모든 단어를 길이마다 나누어서 저장하기 위한 리스트
+array = [[] for _ in range(10001)]
+# 모든 단어를 길이마다 나누어서 뒤집어 저장하기 위한 리스트
+reversed_array = [[] for _ in range(10001)]
+
+def solution(words, queries):
+    answer = []
+    for word in words: # 모든 단어를 접미사 와일드카드 배열, 접두사 와일드카드 배열에 각각 삽입
+        array[len(word)].append(word) # 단어를 삽입
+        reversed_array[len(word)].append(word[::-1]) # 단어를 뒤집어서 삽입
+        
+    for i in range(10001): #이진 탐색을 수행하기 위해 각 단어 리스트 정렬 수행
+        array[i].sort()
+        reversed_array[i].sort()
+                
+    for q in queries: # 쿼리를 하나씩 확인하며 처리
+        if q[0] != '?': # 접미사에 와일드카드가 붙은 경우
+            res = count_by_range(array[len(q)], q.replace('?', 'a'), q.replace('?', 'z'))
+        else: # 접두사에 와일드카드가 붙은 경우
+            res = count_by_range(reversed_array[len(q)], q[::-1].replace('?', 'a'), q[::-1].replace('?', 'z'))
+        # 검색된 단어의 개수를 저장
+        answer.append(res)
+    return answer
+
+
+words = input().split()
+queries = input().split()
+
+print(solution(words, queries))
 
