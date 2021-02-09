5df485cd2856883419b00dab8918ba700e9dabb4
ukunV <showklee@naver.com>
2021-02-09 13:26:01 +0900

    21/02/09_add
    21/02/09_add

---
+++ this_is_coding_test/Chapter17_Shortest_Path_problems.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <b> 37. 플로이드
+
+# ans)
+
+# In[ ]:
+
+
+INF = int(1e9) # 무한을 의미하는 값으로 10억을 설정
+
+# 노드의 개수 및 간선의 개수를 입력받기
+n = int(input())
+m = int(input())
+# 2차원 리스트(그래프 표현)를 만들고, 모든 값을 무한으로 초기화
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+# 자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if a == b:
+            graph[a][b] = 0
+            
+# 각 간선에 대한 정보를 입력받아, 그 값으로 초기화
+for _ in range(m):
+    # A에서 B로 가는 비용은 C라고 설정
+    a, b, c = map(int, input().split())
+    # 가장 짧은 간선 정보만 저장
+    if c < graph[a][b]:
+        graph[a][b] = c
+        
+# 점화식에 따라 플로이드 워셜 알고리즘을 수행
+for k in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+            
+print("-----------------------------------")
+            
+# 수행된 결과를 출력
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        # 도달할 수 없는 경우, 0을 출력
+        if graph[a][b] == INF:
+            print(0, end=" ")
+        # 도달할 수 있는 경우 거리를 출력
+        else:
+            print(graph[a][b], end=" ")
+    print()
+
+
+# <b> 38. 정확한 순위
+
+# ans)
+
+# In[ ]:
+
+
+INF = int(1e9) # 무한을 의미하는 값으로 10억을 설정
+
+# 노드의 개수, 간선의 개수를 입력받기
+n, m = map(int, input().split())
+# 2차원 리스트(그래프 표현)를 만들고, 모든 값을 무한으로 초기화
+graph =[[INF] * (n + 1) for _ in range(n + 1)]
+
+# 자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if a == b:
+            graph[a][b]
+            
+# 각 간선에 대한 정보를 입력받아, 그 값으로 초기화
+for _ in range(m):
+    # A에서 B로 가는 비용을 1로 설정
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+    
+# 점화식에 따라 플로이드 워셜 알고리즘을 수행
+for k in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+            
+result = 0
+# 각 학생을 번호에 따라 한 명씩 확인하며 도달 가능한지 체크
+for i in range(1, n + 1):
+    count = 0
+    for j in range(1, n + 1):
+        if graph[i][j] != INF or graph[j][i] != INF:
+            count += 1
+        if count == n:
+            result += 1
+print(result)
+
+
+# <b> 39. 화성 탐사
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# <b> 40. 숨바꼭질
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
