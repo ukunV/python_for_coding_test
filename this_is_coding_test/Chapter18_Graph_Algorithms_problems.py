03856b7ce85b303f8d5d1cf76ad12e5c5ed5bff7
ukunV <showklee@naver.com>
2021-02-14 15:21:19 +0900

    21/02/14_update
    21/02/14_update

--- this_is_coding_test/Chapter18_Graph_Algorithms_problems.py
+++ this_is_coding_test/Chapter18_Graph_Algorithms_problems.py
@@ -102,42 +102,218 @@ print(result)
 
 # <b> 43. 어두운 길
 
-# In[ ]:
-
-
-
-
+# ans)
 
 # In[ ]:
 
 
+# 특정 원소가 속한 집합을 찾기
+def find_parent(parent, x):
+    # 루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
 
+# 두 원소가 속한 집합을 합치기
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+        
+# 노드의 개수와 간선의 개수 입력받기
+n, m = map(int, input().split())
+parent = [0] * (n + 1) # 부모 테이블 초기화
+
+# 모든 간선을 담을 리스트와 최종 비용을 담을 변수
+edges = []
+result = 0
+
+# 부모 테이블상에서, 부모를 자기 자신으로 초기화
+for i in range(1, n + 1):
+    parent[i] = i
+    
+# 모든 간선에 대한 정보를 입력받기
+for _ in range(m):
+    x, y, z = map(int, input().split())
+    # 비용순으로 정렬하기 위해서 튜플의 첫 번째 원소를 비용으로 설정
+    edges.append((z, x, y))
+
+# 간선을 비용순으로 정렬
+edges.sort()
+total = 0 # 전체 가로등 비용
+
+# 간선을 하나씩 확인하며
+for edge in edges:
+    cost, a, b = edge
+    total += cost
+    # 사이클이 발생하지 않는 경우에만 집합에 포함
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+        
+print(total - result)
 
 
 # <b> 44. 행성 터널
 
-# In[ ]:
+# ans)
+
+# In[2]:
 
 
+# 특정 원소가 속한 집합을 찾기
+def find_parent(parent, x):
+    # 루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
 
+# 두 원소가 속한 집합을 합치기
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+        
+# 노드의 개수 입력받기
+n = int(input())
+parent = [0] * (n + 1) # 부모 테이블 초기화
 
+# 모든 간선을 담을 리스트와 최종 비용을 담을 변수
+edges = []
+result = 0
 
-# In[ ]:
+# 부모 테이블 상에서 부모를 자기 자신으로 초기화
+for i in range(1, n + 1):
+    parent[i] = i
+    
+x = []
+y = []
+z = []
 
-
-
+# 모든 노드에 대한 좌표 값 입력받기
+for i in range(1, n + 1):
+    data = list(map(int, input().split()))
+    x.append((data[0], i))
+    y.append((data[1], i))
+    z.append((data[2], i))
+    
+x.sort()
+y.sort()
+z.sort()
+print(x)
+print(y)
+print(z)
+# 인접한 노드들로부터 간선 정보를 추출하여 처리
+for i in range(n - 1):
+    # 비용순으로 정렬하기 위해서 튜플의 첫 번째 원소를 비용으로 설정
+    edges.append((x[i + 1][0] - x[i][0], x[i][1], x[i + 1][1]))
+    edges.append((y[i + 1][0] - y[i][0], y[i][1], y[i + 1][1]))
+    edges.append((z[i + 1][0] - z[i][0], z[i][1], z[i + 1][1]))
+    
+# 간선을 비용순으로 정렬
+print(edges)
+edges.sort()
+print(edges)
+# 간선을 하나씩 확인하며
+for edge in edges:
+    cost, a, b = edge
+    # 사이클이 발생하지 않는 경우에만 집합에 포함
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+        
+print(result)
 
 
 # <b> 45. 최종 순위
 
-# In[ ]:
+# ans)
+
+# In[10]:
 
 
+from collections import deque
 
-
-
-# In[ ]:
-
-
-
+# 테스트 케이스(Test Case)만큼 반복
+for tc in range(int(input())):
+    # 노드의 개수 입력받기
+    n = int(input())
+    # 모든 노드에 대한 진입차수는 0으로 초기화
+    indegree = [0] * (n + 1)
+    # 각 노드에 연결된 간선 정보를 담기 위한 인접 행렬 초기화
+    graph = [[False] * (n + 1) for i in range(n + 1)]
+    # 작년 순위 정보 입력
+    data = list(map(int, input().split()))
+    # 방향 그래프와 간선 정보 초기화
+    for i in range(n):
+        for j in range(i + 1, n):
+            graph[data[i]][data[j]] = True
+            indegree[data[j]] += 1
+            
+    # 올해 변경된 순위 정보 입력
+    m = int(input())
+    for i in range(m):
+        a, b = map(int, input().split())
+        # 간선의 방향 뒤집기
+        if graph[a][b]:
+            graph[a][b] = False
+            graph[b][a] = True
+            indegree[a] += 1
+            indegree[b] -= 1
+        else:
+            graph[a][b] = True
+            graph[b][a] = False
+            indegree[a] -= 1
+            indegree[b] += 1
+            
+    # 위상 정렬(Topology Sort) 시작
+    result = [] # 알고리즘 수행 결과를 담을 리스트
+    q = deque() # 큐 기능을 위한 deque 라이브러리 사용
+    
+    # 처음 시작할 때는 진입차수가 0인 노드를 큐에 삽입
+    for i in range(1, n + 1):
+        if indegree[i] == 0:
+            q.append(i)
+            
+    certain = True # 위상 정렬 결과가 오직 하나인지의 여부
+    cycle = False # 그래프 내 사이클이 존재하는지 여부
+    
+    # 정확히 노드의 개수만큼 반복
+    for i in range(n):
+        # 큐가 비어 있다면 사이클이 발생했다는 의미
+        if len(q) == 0:
+            cycle = True
+            break
+        # 큐의 원소가 2개 이상이라면 가능한 정렬 결과가 여러 개라는 의미
+        if len(q) >= 2:
+            certain = False
+            break
+        # 큐에서 원소 꺼내기
+        now = q.popleft()
+        result.append(now)
+        # 해당 원소와 연결된 노드들의 진입차수에서 1 빼기
+        for i in range(1, n + 1):
+            if graph[now][i]:
+                indegree[i] -= 1
+                # 새롭게 진입차수가 0이 되는 노드를 큐에 삽입
+                if indegree[i] == 0:
+                    q.append(i)
+                    
+    # 사이클이 발생하는 경우(일관성이 없는 경우)
+    if cycle:
+        print('IMPOSSIBLE')
+    # 위상 정렬 결과가 여러 개인 경우
+    elif not certain:
+        print('?')
+    # 위상 정렬을 수행한 결과 출력
+    else:
+        for i in result:
+            print(i, end=' ')
+        print()
 
