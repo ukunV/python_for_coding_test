6041fce232c8b1e2f1903871ae73d11df452b8d9
ukunV <showklee@naver.com>
2021-04-04 17:49:31 +0900

    add problem

--- programmers_level_1/level_1_완주하지 못한 선수.py
+++ programmers_level_1/level_1_완주하지 못한 선수.py
@@ -1,20 +1,35 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# <b>Level 1_완주하지 못한 선수(*)
+# <b>Level 1_완주하지 못한 선수
 
 # In[ ]:
 
 
 def solution(participant, completion):
-    answer = ''
-    participant.sort()
-    completion.sort()
-    
-    for i in range(len(completion)):
-        if participant[i] != completion[i]:
-            answer = participant[i]
-            return answer
-    answer = participant[len(participant) - 1]        
-    return answer
+    who = ""
+    for p in participant:
+        if p not in completion:
+            who = p
+            break
+        completion.remove(p)
+            
+    return who
+
+print(solution(['a', 'c', 'b', 'a'], ['a', 'b', 'c']))
+
+
+# ans)
+
+# In[ ]:
+
+
+import collections
+
+def solution(participant, completion):
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    print(answer)
+    return list(answer.keys())[0]
+
+print(solution(['a', 'c', 'b', 'a'], ['a', 'b', 'c']))
 
