182a98744db4f6066ab7002449d705b73fd0d076
ukunV <showklee@naver.com>
2021-04-16 09:54:30 +0900

    modify problem

--- programmers_level_2/level_2_124 나라의 숫자.py
+++ programmers_level_2/level_2_124 나라의 숫자.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# <b>Level 2_124 나라의 숫자(*)
+# <b>Level 2_124 나라의 숫자
+
+# ans)
 
 # In[1]:
 
@@ -14,4 +16,33 @@ def solution(n):
         return solution(q) + '124'[r]
 
 print(solution(20))
+
+
+# In[ ]:
+
+
+def solution(n):
+    numbers = ['4', '1', '2']
+    answer = ''
+    
+    while n:
+        answer = numbers[n % 3] + answer
+        n = n // 3 - (n % 3 == 0)
+        
+    return answer
+
+
+# In[ ]:
+
+
+def solution(n):
+    num = ['1','2','4']
+    answer = ""
+
+    while n > 0:
+        n -= 1
+        answer = num[n % 3] + answer
+        n //= 3
+
+    return answer
 
