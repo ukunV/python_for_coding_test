9f41247171852d54001823d548e314e19cc231bb
ukunV <showklee@naver.com>
2021-04-26 21:58:52 +0900

    review problem

--- programmers_level_2/level_2_프린터.py
+++ programmers_level_2/level_2_프린터.py
@@ -1,29 +1,57 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# <b>Level 2_프린터(*)
+# <b> level_2_프린터
 
-# In[1]:
+# In[11]:
 
+
+from collections import deque
 
 def solution(priorities, location):
     answer = 0
-    s = sorted(priorities, reverse = True)
-    ck = 0
-    while 1:
-        for i, j in enumerate(priorities):
-            if j == s[ck]:
-                ck += 1
-                answer += 1
-                if i == location:
-                    break
+    
+    q = deque([(i, j) for i, j in enumerate(priorities)])
+    
+    result = []
+    while q:
+        if q[0][1] == max(q, key=lambda x:x[1])[1]:
+            result.append(q.popleft())
         else:
-            continue
+            tmp = q.popleft()
+            q.append(tmp)
             
-        break
+    for i in range(len(result)):
+        if result[i][0] == location:
+            answer = i + 1
+            break
+            
     return answer
 
-print(solution([2,1,3,2],2))
-print(solution([1,1,9,1,1,1],0))
-print(solution([9,9,1,9,9,9],2))
+print(solution([2,1,3,2], 2))
+
+
+# ans)
+
+# In[18]:
+
+
+from collections import deque
+
+def solution(priorities, location):
+    answer = 0
+    
+    q = deque([(i,p) for i,p in enumerate(priorities)])
+    
+    while True:
+        print(q)
+        cur = q.popleft()
+        if any(cur[1] < i[1] for i in q):
+            q.append(cur)
+        else:
+            answer += 1
+            if cur[0] == location:
+                return answer
+            
+print(solution([2,1,3,2], 1))
 
