dca9671d8b007827d5bafd25c7a7977a4ab77cff
ukunV <showklee@naver.com>
2021-04-27 14:56:18 +0900

    modify

--- this_is_coding_test/Chapter08_Dynamic_Programming_problems.py
+++ this_is_coding_test/Chapter08_Dynamic_Programming_problems.py
@@ -14,7 +14,7 @@ x = int(input())
 # 앞서 계산된 결과를 저장하기 위한 DP 테이블 초기화
 d = [0] * 30001
 
-# 다이나믹 프로그래밍(Dynamic Programming) 진행(보텀업)
+# 다이나믹 프로그래밍(Dynamic Programming) 진행(Bottom Up)
 for i in range(2, x + 1):
     # 현재의 수에서 1을 빼는 경우
     d[i] = d[i - 1] + 1
@@ -46,7 +46,7 @@ array = list(map(int, input().split()))
 # 앞서 계산된 결과를 저장하기 위한 DP 테이블 초기화
 d = [0] * 100
 
-# 다이나믹 프로그래밍(Dynamic Programming) 진행(보텀업)
+# 다이나믹 프로그래밍(Dynamic Programming) 진행(Bottom Up)
 d[0] = array[0]
 d[1] = max(array[0], array[1])
 for i in range(2, n):
@@ -69,7 +69,7 @@ n = int(input())
 # 앞서 계산된 결과를 저장하기 위한 DP 테이블 초기화
 d[0] * 1001
 
-# 다이나믹 프로그래밍(Dynamic Programming) 진행(보텀업)
+# 다이나믹 프로그래밍(Dynamic Programming) 진행(Bottom Up)
 d[1] = 1
 d[2] = 3
 for i in range(3, n+1):
@@ -99,7 +99,7 @@ for i in range(n):
 # 한 번 계산된 결과를 저장하기 위한 DP 테이블 초기화
 d = [10001] * (m + 1)
 
-# 다이나믹 프로그래밍(Dynamic Programming) 진행(보텀업)
+# 다이나믹 프로그래밍(Dynamic Programming) 진행(Bottom Up)
 d[0] = 0
 for i in range(n):
     for j in range(array[i], m + 1):
