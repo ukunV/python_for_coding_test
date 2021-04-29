22e6c659e69369bcf53044c7320ecb633328df40
ukunV <showklee@naver.com>
2021-04-29 13:32:02 +0900

    add problem

---
+++ programmers_level_2/level_2_다리를 지나는 트럭.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <b> level_2_다리를 지나는 트럭
+
+# In[ ]:
+
+
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+    
+    q = [0] * bridge_length
+    
+    time = 0
+    while q:
+        time += 1
+        q.pop(0)
+        if truck_weights:
+            if sum(q) + truck_weights[0] <= weight:
+                q.append(truck_weights[0])
+                truck_weights.pop(0)
+            else:
+                q.append(0)
+                
+    answer = time    
+    return answer
+
+
+# ans)
+
+# In[ ]:
+
+
+from collections import deque
+
+def solution(bridge_length, weight, truck_weights):
+    bridge = deque(0 for _ in range(bridge_length))
+    total_weight = 0
+    step = 0
+    truck_weights.reverse()
+
+    while truck_weights:
+        total_weight -= bridge.popleft()
+        if total_weight + truck_weights[-1] > weight:
+            bridge.append(0)
+        else:
+            truck = truck_weights.pop()
+            bridge.append(truck)
+            total_weight += truck
+        step += 1
+
+    step += bridge_length
+
+    return step
+
+
+# In[3]:
+
+
+import collections
+
+DUMMY_TRUCK = 0
+
+
+class Bridge(object):
+
+    def __init__(self, length, weight):
+        self._max_length = length
+        self._max_weight = weight
+        self._queue = collections.deque()
+        self._current_weight = 0
+
+    def push(self, truck):
+        next_weight = self._current_weight + truck
+        if next_weight <= self._max_weight and len(self._queue) < self._max_length:
+            self._queue.append(truck)
+            self._current_weight = next_weight
+            return True
+        else:
+            return False
+
+    def pop(self):
+        item = self._queue.popleft()
+        self._current_weight -= item
+        return item
+
+    def __len__(self):
+        return len(self._queue)
+
+    def __repr__(self):
+        return 'Bridge({}/{} : [{}])'.format(self._current_weight, self._max_weight, list(self._queue))
+
+
+def solution(bridge_length, weight, truck_weights):
+    bridge = Bridge(bridge_length, weight)
+    trucks = collections.deque(w for w in truck_weights)
+
+    for _ in range(bridge_length):
+        bridge.push(DUMMY_TRUCK)
+
+    count = 0
+    while trucks:
+        bridge.pop()
+
+        if bridge.push(trucks[0]):
+            trucks.popleft()
+        else:
+            bridge.push(DUMMY_TRUCK)
+
+        count += 1
+
+    while bridge:
+        bridge.pop()
+        count += 1
+
+    return count
+
+
+def main():
+    print(solution(2, 10, [7, 4, 5, 6]))
+    print(solution(100, 100, [10]))
+    print(solution(100, 100, [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]))
+
+
+if __name__ == '__main__':
+    main()
+
