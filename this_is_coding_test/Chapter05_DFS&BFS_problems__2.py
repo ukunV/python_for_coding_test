5ebef55f4e3b07cba0e24be87c39610fa03e85f9
ukunV <showklee@naver.com>
2021-07-03 18:40:28 +0900

    review: DFS&BFS_problems(2)

--- this_is_coding_test/Chapter05_DFS&BFS_problems__2.py
+++ this_is_coding_test/Chapter05_DFS&BFS_problems__2.py
@@ -78,7 +78,7 @@ print(result)
 
 # try)
 
-# In[6]:
+# In[3]:
 
 
 from collections import deque
@@ -97,7 +97,7 @@ def bfs(x, y):
     queue.append((x, y))
     
     while queue:
-        x, y = queue.popleft((x, y))
+        x, y = queue.popleft()
         
         for i in range(4):
             nx = x + dx[i]
@@ -109,7 +109,7 @@ def bfs(x, y):
             if graph[nx][ny] == 1:
                 graph[nx][ny] = graph[x][y] + 1
                 queue.append((nx, ny))
-        return graph[n - 1][m - 1]
+    return graph[n - 1][m - 1]
                 
 print(bfs(0, 0))
 
